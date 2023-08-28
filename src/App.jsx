de73f178974a637799f06df6b11c1d0ac9a6da28
GregNasir <63434657+GregNasir@users.noreply.github.com>
2023-08-28 02:22:07 -0400

    added styling to home page and linked to about page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './components/Home';
+import About from './components/About';
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />} >
           <Route index element={<Home />} />
+          <Route path='about' element={<About/>} />
         </Route>
       </Routes>
     </>
